cdd99614eff1701ecb74a559b70f00bd2fbb9d6c
Vladyslav <vladyslav.nasadiuk@sigma.software>
2019-03-09 18:30:14 +0200

    initial commit

---
+++ src/app/components/empty-form/empty-form.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-empty-form',
+  templateUrl: './empty-form.component.html',
+  styleUrls: ['./empty-form.component.scss']
+})
+export class EmptyFormComponent {
+
+}
+
+
+
